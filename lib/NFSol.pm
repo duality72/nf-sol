6bf95e908c2c303c1bfda420f1162ec648fded11
Chris Hilton <chris@dctank.com>
2013-12-08 17:59:45 -0800

    Added the @REGIONS and %REGIONS data about the various deploy regions

--- lib/NFSol.pm
+++ lib/NFSol.pm
@@ -2,12 +2,33 @@ use strict;
 use warnings;
 use Time::ParseDate;
 use Time::Local;
+use NFSol::Region;
 
 package NFSol;
 
 $NFSol::VERSION = '0.1';
 
 $ENV{TZ} = "America/Los_Angeles";
+
+our @REGIONS = ('US-West', 'US-East', 'EU');
+# TODO: Account for DST for US-West and East
+our %REGIONS = (
+  'US-West' => new NFSol::Region(
+    name          => 'US-West',
+    peakStartHour => 16,
+    peakEndHour   => 7,
+  ),
+  'US-East' => new NFSol::Region(
+    name          => 'US-East',
+    peakStartHour => 19,
+    peakEndHour   => 10,
+  ),
+  'EU' => new NFSol::Region(
+    name          => 'EU',
+    peakStartHour => 0,
+    peakEndHour   => 15,
+  ),
+);
 
 1;
 
--- t/NFSol.t
+++ t/NFSol.t
@@ -9,3 +9,10 @@ BEGIN { use_ok('NFSol') };
 # Verify module can be included via "require" pragma
 require_ok( 'NFSol' );
 
+# @REGIONS and %REGIONS
+# Test that there are three regions defined
+is(scalar @NFSol::REGIONS, 3, "Three regions defined in array");
+foreach my $region (@NFSol::REGIONS) {
+  isa_ok($NFSol::REGIONS{$region}, 'NFSol::Region', "Region $region defined");
+}
+
